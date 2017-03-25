8af60a0876db268d1eddfca748f22afa1ac7588e
Konrad Feiler <K_W_F@web.de>
2017-03-25 14:43:45 +0100

    lazy Iterator so faster when early aborting sequence

--- Pod/Classes/KDTree+Sequence.swift
+++ Pod/Classes/KDTree+Sequence.swift
@@ -10,30 +10,41 @@ import Foundation
 
 extension KDTree: Sequence {
     
-//    public func makeIterator() -> KDTreeIterator {
     public func makeIterator() -> AnyIterator<Element> {
         
         switch self {
         case .leaf: return AnyIterator { return nil }
         case let .node(left, value, _, right):
             var index = 0
-            var leftIterator: AnyIterator<Element>? = left.makeIterator()
-            var rightIterator: AnyIterator<Element>? = right.makeIterator()
+            var iteratorStruct = KDTreeIteratorStruct(left: left, right: right)
 
             return AnyIterator {
                 if index == 0 {
                     index = 1
                     return value
                 }
-                else if index == 1, let nextValue = leftIterator?.next() {
+                else if index == 1, let nextValue = iteratorStruct.leftIterator?.next() {
                     return nextValue
                 }
                 else {
                     index = 2
-                    return rightIterator?.next()
+                    return iteratorStruct.rightIterator?.next()
                 }
             }
             
         }
     }
+}
+
+fileprivate struct KDTreeIteratorStruct<Element: KDTreePoint> {
+    let left: KDTree<Element>
+    let right: KDTree<Element>
+    
+    init(left: KDTree<Element>, right: KDTree<Element>) {
+        self.left = left
+        self.right = right
+    }
+    
+    lazy var leftIterator: AnyIterator<Element>? = { return self.left.makeIterator() }()
+    lazy var rightIterator: AnyIterator<Element>? = { return self.right.makeIterator() }()
 }
