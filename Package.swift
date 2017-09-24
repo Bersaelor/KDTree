35f75ce639fbd19b8ef3236f2d7e59e9ed2e5f0f
Konrad Feiler <K_W_F@web.de>
2017-09-24 22:04:00 +0200

    KDTree is a library not executable for SwiftPM

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "KDTree",
     products: [
-        .executable(
+        .library(
             name: "KDTree",
             targets: ["KDTree"]
         )
