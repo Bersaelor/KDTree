224ee9ca792c55f94264afb3ac404860c4f9f6c1
Chris Eidhof <chris@eidhof.nl>
2017-11-13 08:12:42 +0100

    Re-add tests

--- Package.swift
+++ Package.swift
@@ -14,6 +14,11 @@ let package = Package(
         .target(
             name: "KDTree",
             path: "Sources"
+        ),
+        .testTarget(
+            name: "Tests",
+            dependencies: ["KDTree"],
+            path: "Tests"
         )
     ]
 )
